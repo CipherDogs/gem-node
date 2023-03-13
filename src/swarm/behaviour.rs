d0934f49b1747262276bf17dceea10207698cc8f
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2023-03-13 11:16:51 +0700

    Added swarm

---
+++ src/swarm/behaviour.rs
@@ -0,0 +1,56 @@
+use crate::CARGO_PKG_VERSION;
+use libp2p::{gossipsub, identify, identity, mdns, swarm::NetworkBehaviour, PeerId};
+use std::error::Error;
+
+#[derive(NetworkBehaviour)]
+#[behaviour(out_event = "BehaviourEvent")]
+pub struct Behaviour {
+    pub gossipsub: gossipsub::Behaviour,
+    pub identify: identify::Behaviour,
+    pub mdns: mdns::async_io::Behaviour,
+}
+
+impl Behaviour {
+    pub async fn new(
+        local_key: identity::Keypair,
+        local_peer_id: PeerId,
+    ) -> Result<Self, Box<dyn Error>> {
+        let protocol_version = format!("gem/{}", CARGO_PKG_VERSION);
+
+        Ok(Self {
+            gossipsub: gossipsub::Behaviour::new(
+                gossipsub::MessageAuthenticity::Signed(local_key.clone()),
+                gossipsub::ConfigBuilder::default().build()?,
+            )?,
+            identify: identify::Behaviour::new(identify::Config::new(
+                protocol_version,
+                local_key.public(),
+            )),
+            mdns: mdns::async_io::Behaviour::new(mdns::Config::default(), local_peer_id)?,
+        })
+    }
+}
+
+pub enum BehaviourEvent {
+    Gossipsub(gossipsub::Event),
+    Mdns(mdns::Event),
+    Identify(identify::Event),
+}
+
+impl From<gossipsub::Event> for BehaviourEvent {
+    fn from(event: gossipsub::Event) -> Self {
+        Self::Gossipsub(event)
+    }
+}
+
+impl From<mdns::Event> for BehaviourEvent {
+    fn from(event: mdns::Event) -> Self {
+        Self::Mdns(event)
+    }
+}
+
+impl From<identify::Event> for BehaviourEvent {
+    fn from(event: identify::Event) -> Self {
+        Self::Identify(event)
+    }
+}
---
+++ src/swarm/mod.rs
@@ -0,0 +1,37 @@
+pub mod behaviour;
+
+use behaviour::Behaviour;
+use libp2p::{
+    core::transport::upgrade::Version, gossipsub, identity, mplex, noise, tcp, PeerId, Swarm,
+    Transport,
+};
+use std::{error::Error, time::Duration};
+
+pub async fn init() -> Result<Swarm<Behaviour>, Box<dyn Error>> {
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+
+    let transport = tcp::async_io::Transport::new(tcp::Config::default().nodelay(true))
+        .upgrade(Version::V1)
+        .authenticate(noise::NoiseAuthenticated::xx(&local_key.clone()).unwrap())
+        .multiplex(mplex::MplexConfig::new())
+        .timeout(Duration::from_secs(20))
+        .boxed();
+
+    let mut behaviour = Behaviour::new(local_key, local_peer_id).await?;
+
+    for ident_topic in [
+        gossipsub::IdentTopic::new("block"),
+        gossipsub::IdentTopic::new("transaction"),
+    ]
+    .iter()
+    {
+        behaviour.gossipsub.subscribe(ident_topic)?;
+    }
+
+    Ok(Swarm::with_async_std_executor(
+        transport,
+        behaviour,
+        local_peer_id,
+    ))
+}
