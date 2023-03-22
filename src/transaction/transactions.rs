85954262988ed4583e1ce31bb19b5b90a97e9bc0
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2023-03-23 00:48:07 +0700

    Fixing a transactions struct

--- src/transaction/transactions.rs
+++ src/transaction/transactions.rs
@@ -3,21 +3,19 @@ use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
 #[derive(Default, Serialize, Deserialize, Debug)]
-pub struct Transactions {
-    pub transactions: Vec<Transaction>,
-}
+pub struct Transactions(pub Vec<Transaction>);
 
 impl Transactions {
     /// Getting the number of transactions
     pub fn len(&self) -> u64 {
-        self.transactions.len() as u64
+        self.0.len() as u64
     }
 
     /// Getting a Vec<Hash> vector of transaction hashes
     pub fn to_vec_hash(&self) -> Result<Vec<Hash>> {
         let mut result = vec![];
 
-        for transaction in self.transactions.iter() {
+        for transaction in self.0.iter() {
             let hash = transaction.hash()?;
 
             result.push(hash);
@@ -30,7 +28,7 @@ impl Transactions {
     pub fn to_vec_hash_bytes(&self) -> Result<Vec<u8>> {
         let mut result = vec![];
 
-        for transaction in self.transactions.iter() {
+        for transaction in self.0.iter() {
             let hash = transaction.hash()?;
 
             result.extend_from_slice(&hash);
@@ -41,6 +39,6 @@ impl Transactions {
 
     /// Adding a transaction
     pub fn push(&mut self, transaction: Transaction) {
-        self.transactions.push(transaction);
+        self.0.push(transaction);
     }
 }
